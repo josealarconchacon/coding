4bb906b91c1474ced1db6e29966938a4f2a48632
josealarconchacon <jose.alarcon.chacon@gmail.com>
2025-02-04 18:23:42 -0500

    print the the length of the phrase

---
+++ cics-hc/string_lengths.py
@@ -0,0 +1,11 @@
+
+# Loop 5 times
+for i in range(5):
+    # ask the user to enter a phrase
+    enter_phrase = input("Enter phrase: ")
+    # print phrase
+    print(enter_phrase)
+    # print the the length of the phrase
+    print(f"Length is: {len(enter_phrase)}")
+print("Done.")
+    
