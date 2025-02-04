6c52f1116392f00633ad36e504dae01e5648dcfb
josealarconchacon <jose.alarcon.chacon@gmail.com>
2025-02-04 15:50:28 -0500

    uppet lower case

---
+++ cics-hc/uppet_lower.py
@@ -0,0 +1,8 @@
+# as user to enter a message, then convert that message to Upper and Lower case
+message = input("Enter a message, then see that message convert to:\n(Upper & Lower) case:\nMessage: ")
+# print the message
+print(message)
+# print the message in uppercase
+print(message.upper())
+# print the message in lowercase
+print(message.lower())
