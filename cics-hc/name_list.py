942416eb99fbd52797d698720db2c63d8ceb57b6
josealarconchacon <jose.alarcon.chacon@gmail.com>
2025-02-04 17:32:56 -0500

    name list

--- cics-hc/name_list.py
+++ cics-hc/name_list.py
@@ -1,11 +1,19 @@
-# ass user to enter a list of names
-names = input("Enter a list of names separated by semicolons (;):\n"
-                    "- Example: Doe, John; Smith, Alice\n"
-                    "- Format: LastName, FirstName\n"
-                    "Enter names: ")
-# print the name one per line
-for name in names.split(';'):
-    # get the fist character for the fist name
-    first_name = name.split(',')[1].split()[0][0] + '.'
-    # print the name
-    print(f"{first_name} {name.split(',')[0]}")  # print the name
+# Ask user to enter a list of names
+enter_names = input("Enter a list of names separated by semicolons (;):\n"
+              "- Example: Lastname, Firstname; Lastname, Firstname\n"
+              "Enter names: ")
+
+# loop over each name , splitting by (;) one per line
+for name in enter_names.split("; "):
+    # split each name by ', ' to separate last and first names
+    split_name = name.split(", ")
+
+    last_name = split_name[0] # set a new last_name variable to store last_name
+    first_name = split_name[1] # set a new first_name variable to store first_name
+
+    # Print the first initial of the first name, followed by ".", and followed by the last name.
+    print(f"{first_name[0]}. {last_name}")
+
+print("\nThank you for using my name organizer!")
+
+    
