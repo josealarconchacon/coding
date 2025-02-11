20129453ee54f7c3a908c576b9554d84ad20958b
josealarconchacon <jose.alarcon.chacon@gmail.com>
2025-02-10 22:31:02 -0500

    colot stamp

--- cics-hc/color_stamps.py
+++ cics-hc/color_stamps.py
@@ -1,18 +1,16 @@
+
 import turtle
 
 # Create a turtle object
 t_color_stamps = turtle.Turtle()
+t_color_stamps.backward(-15)
 
 # Loop for 5 colors
 for i in range(5):
-     # Get color input
-    color = input("Enter color (as hex): ") 
-    # stamps out that color using the turtle library
-    t_color_stamps.color(color)
-    t_color_stamps.fillcolor(color)
-
-    t_color_stamps.stamp()  # Stamp at current position
+    color = input("Enter color (as hex): ")  # Get color input
     t_color_stamps.forward(20)  # Move forward for spacing
+    t_color_stamps.color(color) # stamps out that color using the turtle library
+    t_color_stamps.stamp()  # Stamp at current position
 
 # Keep the window open
 turtle.done()
