208bf78bab75fbb362c1d956349aeea86127099d
josealarconchacon <jose.alarcon.chacon@gmail.com>
2025-03-08 17:46:55 -0500

    majority_circuit

---
+++ cics-hc02/majority_circuit.py
@@ -0,0 +1,4 @@
+# The output is True if at least two of the inputs (in1, in2, in3) are True.
+# We achieve this by checking all possible pairs of True values.
+
+out = (in1 and in2) or (in1 and in3) or (in2 and in3)
