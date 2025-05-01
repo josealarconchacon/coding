ab870ec8b1eae54b92ee82e89ebc39b4e7b1dd3b
josealarconchacon <jose.alarcon.chacon@gmail.com>
2025-05-01 16:40:50 -0400

    test

---
+++ cics-hc05/main.cpp
@@ -0,0 +1,11 @@
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    // print Hello, C++!
+    cout << "Hello, C++!" << endl;
+    // print Hello, Thomas Hunter
+    cout << "Hello, Thomas Hunter"<< endl;
+    return 0;
+}
